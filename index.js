04cebeaaf17c7a080d0827a43750681f43b64484
AnzhelikaMullova <ulanova592@gmail.com>
2023-03-12 14:53:58 +0300

    DZ

---
+++ index.js
@@ -0,0 +1,45 @@
+let salary = 40000;
+let salary1 =22000;
+console.log(salary + salary1); 62000
+
+let song = "Владивосток";
+let nam = 2000;
+console.log(song + " " + nam); 
+
+
+let min = 128;
+let min2 =254;
+let min3 =137;
+let min4 =201;
+
+console.log((min +min2+min3+min4)/60);
+
+
+let a =2;
+console.log(a**5);
+console.log('Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!')
+
+
+const greetings = "Привет";
+const name1 = "Кот";
+console.log(greetings + "," + name1);
+
+let time=0;
+console.log(time);
+console.log('Старт поездки. Осталось минут: ' + time);
+
+console.log('Немного сторис в соцсетях. Осталось минут: ' + time);
+
+console.log('Немного не новостей, но событий. Осталось минут: ' + time);
+
+console.log('Вы приехали. Добро пожаловать в Москву ');
+
+
+let  fahrenheit=451;
+
+let celsius=235.7;
+console.log(`${fahrenheit} градуса по Фаренгейту — это ${celsius} градуса по Цельсию.`);
+
+
+
+
